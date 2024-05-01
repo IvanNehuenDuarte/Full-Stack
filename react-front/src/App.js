11a3b73907c5845750e0adf3e377f7477755e3cb
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-01 19:38:36 -0300

    Utilizamos manejo de versiones para solucionar problemas de compatibilidad entre versiones de alguna dependencia con nodejs. Creamos el componente ShowBlogs

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className="btn btn-primary btn-lg" ><i className='fab fa-react'></i></button>
       </header>
     </div>
   );
